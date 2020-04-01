b012f35b37fce1391f1f26e7e5db59ae0510443d
tjr127 <ty.rost@rostlabs.com>
2020-04-01 12:19:32 -0400

    lab 5

--- 5/names.py
+++ 5/names.py
@@ -9,12 +9,13 @@ PY_CONTENT_CREATORS = ['brian okken', 'michael kennedy', 'trey hunner',
 
 
 def dedup_and_title_case_names(names):
-    newlist = []
+    return list({name.title() for name in names})
+'''    newlist = []
     for name in names:
         name = name.title()
         if name not in newlist:
             newlist.append(name)
-    return(newlist)
+    return(newlist)'''
 
     """Should return a list of title cased names,
        each name appears only once"""
@@ -33,16 +34,43 @@ def shortest_first_name(names):
     """Returns the shortest first name (str).
        You can assume there is only one shortest name.
     """
-    shortname = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ '''   shortname = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     names = dedup_and_title_case_names(names)
     for name in names:
         firstname = name.split(" ")[0]
         if len(firstname)<len(shortname):
             shortname=firstname
-    return(shortname)
+    return(shortname)'''
+    names = dedup_and_title_case_names(names)
+    names = [name.split()[0] for name in names]
+    return min(names, key=len)
 
 if __name__=="__main__":
     print(dedup_and_title_case_names(NAMES))
     print(sort_by_surname_desc(NAMES))
     print(sort_by_surname_desc(PY_CONTENT_CREATORS))
     print(shortest_first_name(NAMES))
+
+'''
+def dedup_and_title_case_names(names):
+    """Should return a list of title cased names,
+       each name appears only once"""
+    return list({name.title() for name in names})
+
+
+def sort_by_surname_desc(names):
+    """Returns names list sorted desc by surname"""
+    names = dedup_and_title_case_names(names)
+    return sorted(names,
+                  key=lambda x: x.split()[-1],
+                  reverse=True)
+
+
+def shortest_first_name(names):
+    """Returns the shortest first name (str).
+       You can assume there is only one shortest name.
+    """
+    names = dedup_and_title_case_names(names)
+    names = [name.split()[0] for name in names]
+    return min(names, key=len)
+'''
